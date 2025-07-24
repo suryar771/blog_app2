f61c7588ce5e4062ff3b28068dadcb217b8e021c
suryar771 <suri624177@gmail.com>
2025-07-24 09:53:15 +0530

    model

---
+++ models/user.js
@@ -0,0 +1,37 @@
+const { Schema,model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImageURL:{
+        type :String ,
+
+    },
+    role:{
+        type:String,
+        enum: ["USER","ADMIN"],
+        default: "USER",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const User = model('user', userSchema);
+module.exports = User;

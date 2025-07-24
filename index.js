b14cc5111308beea01a5cc08db0e8f4602b47ae1
suryar771 <suri624177@gmail.com>
2025-07-24 09:35:39 +0530

    EJS home page setup

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const path = require("path");
 const app = express();
 const PORT = 8000;
 
-app.set('view engine','ejs');
+app.set(' view engine','ejs');
 app.set('views',path.resolve("./views"));
-
+app.get('/',(req,res)=>{
+    res.render("home");
+});    
 app.listen(PORT, ()=>{
     console.log(`Server started at 8000`);
 })

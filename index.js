0d0a5b6acc7424196f8bbe097e01c078ba93bbd4
suryar771 <suri624177@gmail.com>
2025-07-24 09:15:35 +0530

    ejs

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require("path");
+
+const app = express();
+const PORT = 8000;
+
+app.set('view engine','ejs');
+app.set('views',path.resolve("./views"));
+
+app.listen(PORT, ()=>{
+    console.log(`Server started at 8000`);
+})

14a0565238151ca9466d4e2795bd62ff45dd6709
suryar771 <suri624177@gmail.com>
2025-07-28 11:31:38 +0530

    Done with EJS and moving to backend

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const PORT = 8000;
 app.set('view engine','ejs');
 app.set('views',path.resolve("./views"));
 
+
+app.get('/',(req,res)=>{
+    res.render("home");
+    
+})
 app.listen(PORT, ()=>{
     console.log(`Sever started at PORT:${PORT}`);
 });

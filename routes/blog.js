71a77353e4bb4f9975f8d133b3f5141e11874a73
suryar771 <suri624177@gmail.com>
2025-07-30 12:45:24 +0530

    added blog pages and moving to render in front end

--- routes/blog.js
+++ routes/blog.js
@@ -1,5 +1,20 @@
 const { Router } = require("express");
+const multer = require('multer');
+const path = require('path');
+
+const Blog = require('../models/blog');
 const router = Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(`./public/uploads/`)); // Update this if req.user is not a string
+    },
+    filename: function (req, file, cb) {
+      const fileName = `${Date.now()}-${file.originalname}`;
+      cb(null, fileName);
+    }
+  });
+const upload = multer({storage: storage});
 
 router.get('/add-new', (req, res) => {
     return res.render("addBlog", {
@@ -7,9 +22,14 @@ router.get('/add-new', (req, res) => {
     });
 });
 
-router.post("/", (req,res)=>{
-    console.log(req.body);
-    return res.redirect("/");
-})
-
+router.post("/", upload.single("coverImage"),async (req,res)=>{
+    const { title, body } = req.body;
+  const blog = await Blog.create({
+    title,
+    body,
+    createdBy: req.user._id,
+    coverImageURL: `/uploads/${req.file.filename}`,
+  });
+  return res.redirect(`/blog/${blog._id}`);
+});
 module.exports = router;
